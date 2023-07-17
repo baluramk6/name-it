3aeb11570a507a3958c99f1e851d66bffeae4363
baluramk6 <baluramk6@gmail.com>
2023-07-17 22:29:22 +0530

    SearchBox error fix

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -4,7 +4,7 @@ import "./SearchBox.css"
 const SearchBox = ({ handleInputChange }) => {
 
     return <div className="search-container">
-        <input type="text" placeholder="Type any keywors..." className="search-input" onChange={(events) => handleInputChange(events.target.value)} />
+        <input type="text" placeholder="Type any keyword..." className="search-input" onChange={(events) => handleInputChange(events.target.value)} />
     </div>
 }
 
